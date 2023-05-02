6edfb9050595bffd2663c37214d2634392aa4c92
robiulXhasan <robi16275@gmail.com>
2023-05-02 18:07:23 +0600

    make duynamic data for specific id

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require("express");
 
 const cors = require("cors");
-const data = require("./data/chefData.json");
+const allData = require("./data/chefData.json");
 
 const app = express();
 app.use(cors());
 const port = process.env.PORT || 5000;
 
+// all data
 app.get("/", (req, res) => {
   res.send("Hello From chef recipe server");
 });
 app.get("/chef", (req, res) => {
-  res.send(data);
+  res.send(allData);
+});
+// specific data
+app.get("/chef/:id", (req, res) => {
+  const id = req.params.id;
+  const specificData = allData.find((data) => data.id == id);
+  res.send(specificData);
 });
 
 app.listen(port, () => {
