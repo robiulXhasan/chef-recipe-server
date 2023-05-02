692e54f6e1c75bce78c40eb0c49efa67f4ef9d66
robiulXhasan <robi16275@gmail.com>
2023-05-02 16:43:24 +0600

    dunamic url make that contain all data

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+const data = require("./data/chefData.json");
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello From chef recipe server");
+});
+app.get("/chef", (req, res) => {
+  res.send(data);
 });
 
 app.listen(port, () => {
